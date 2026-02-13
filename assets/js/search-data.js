0150b90e506003665004155cc6333a6bc85a7051
qhe28 <qidong.he.28@gmail.com>
2026-02-13 23:56:49 +0000

    Deploying to gh-pages from @ qhe28/qhe28.github.io@15b211d5e1aab6ae7f742213ef8d349b7cf54d6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,35 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%71%68%39%37@%6D%61%74%68.%72%75%74%67%65%72%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=MFB--u8AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
